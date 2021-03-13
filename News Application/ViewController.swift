41359d5289103b18ddc8e1f147876787f07896d0
Dmitriy Kutvitskiy <dima.kutvickiy698@gmail.com>
2021-03-13 14:55:21 +0200

    Initial Commit

---
+++ News Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Application
+//
+//  Created by Дмитрий Кутвицкий on 13.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
